4d6636736aaa802df934753684222c8251d78845
mlieshoff <michaellieshoff@gmx.de>
2019-12-15 11:14:46 +0100

    gotya release.

--- apps/gotya/dist.sh
+++ apps/gotya/dist.sh
@@ -7,7 +7,14 @@ cp target/server-1.0.jar apps/gotya/libs
 echo prepare gotya release...
 cd ../gotya
 
-# TODO gradle call
+echo copy production keys to ids.xml
+cp ../server_credentials/conf/gotya/ids.xml android/res/values/ids.xml
+
+echo create jars...
+./gradlew jar
+
+echo TODO gradle call to assemble apk
+# ./gradlew android:assemble
 
 echo encrypt...
 java -cp ../server/libs/server-1.0.jar server.FileCipher ../server_credentials/conf/gotya/key backend/build/libs/backend-1.0.jar code ../server/apps/gotya/libs/gotya-encrypted.jar
