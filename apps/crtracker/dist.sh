15bb7abcf5471642420bfd862242827854611370
mlieshoff <michaellieshoff@gmx.de>
2020-11-12 08:39:55 +0100

    new release of cr-tracker

--- apps/crtracker/dist.sh
+++ apps/crtracker/dist.sh
@@ -9,7 +9,7 @@ cd ../crtracker
 mvn clean install
 
 echo encrypt...
-java -cp ../server/libs/server-1.0.jar server.FileCipher ../server_credentials/conf/crtracker/key target/crtracker-1.0.0-shaded.jar code ../server/apps/crtracker/libs/crtracker-encrypted.jar
+java -cp ../server/libs/server-1.0.jar server.FileCipher ../server_credentials/conf/crtracker/key target/crtracker-1.0.0.jar code ../server/apps/crtracker/libs/crtracker-encrypted.jar
 
 echo copy credentials...
 cp ../server_credentials/conf/crtracker/encrypted/credentials.properties ../server/apps/crtracker/conf
