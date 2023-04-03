54ff854e5c4d23d390b08398c231cef071872b35
mlieshoff <michaellieshoff@gmx.de>
2023-04-03 15:55:40 +0200

    GotYa goes Java 20

--- apps/gotya/dist.sh
+++ apps/gotya/dist.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk use java 20-oracle
+
 echo create server libs...
 mvn clean install
 cp target/server-1.0.jar apps/gotya/libs
@@ -10,14 +14,19 @@ cd ../gotya
 echo copy production keys to ids.xml
 cp ../server_credentials/conf/gotya/ids.xml android/res/values/ids.xml
 
-echo create jars...
-./gradlew clean assemble
+echo create backend jar...
+cd backend
+mvn clean install
+cd ..
+
+echo create game jars...
+# ./gradlew clean assemble
 
 echo TODO gradle call to assemble apk
 # ./gradlew android:assemble
 
 echo encrypt...
-java -cp ../server/libs/server-1.0.jar server.FileCipher ../server_credentials/conf/gotya/key backend/build/libs/backend-1.0.jar code ../server/apps/gotya/libs/gotya-encrypted.jar
+java -cp ../server/libs/server-1.0.jar server.FileCipher ../server_credentials/conf/gotya/key backend/target/backend-1.0.0.jar code ../server/apps/gotya/libs/gotya-encrypted.jar
 
 echo copy credentials...
 cp ../server_credentials/conf/gotya/encrypted/credentials.properties ../server/apps/gotya/conf
--- apps/gotya/release.sh
+++ apps/gotya/release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk use java 20-oracle
+
 DIRECTORY=/home/gotya/server
 STATUS=$DIRECTORY/gotya.pid
 APP=$DIRECTORY/server/apps/gotya
